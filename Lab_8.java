75aab6ed23548098cda8e90a9d1782edb1db0567
aparna <69113624+Aparna-24@users.noreply.github.com>
2020-09-16 18:50:23 +0530

    Add files via upload

--- Lab_8.java
+++ Lab_8.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Venkatesh
+ * @author hp
  */
 public class Lab_8 {
 
--- prg_2.java
+++ prg_2.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 /**
  *
- * @author Venkatesh
+ * @author hp
  */
 public class prg_2 {
 
